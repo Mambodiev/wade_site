3ab99df50cfca06298e0a2b3876e2e548303ea29
mambodieu <mambodieu@gmail.com>
2022-02-24 08:57:32 -0400

    master

--- ouest/settings.py
+++ ouest/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # env = environ.Env()
 # read the .env file
 # environ.Env.read_env()
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".ouestsenegal.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "www.ouestsenegal.herokuapp.com"]
 SECRET_KEY = config('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -116,12 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL='/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../diev/wade_site/static")
+    os.path.join(BASE_DIR, "project/static/")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_URL = '/media/'
