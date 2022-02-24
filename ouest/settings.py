5a9df0be30b3fd83a394ec4d011b799ce3464ea1
mambodieu <mambodieu@gmail.com>
2022-02-24 03:32:56 -0400

    master

--- ouest/settings.py
+++ ouest/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # env = environ.Env()
 # read the .env file
 # environ.Env.read_env()
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 SECRET_KEY = config('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
