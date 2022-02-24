b68002bcf621ad521eb14fb1540011fafd248912
mambodieu <mambodieu@gmail.com>
2022-02-24 12:56:51 -0400

    succes collect static file

--- ouest/urls.py
+++ ouest/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from blog import views
 
 
@@ -16,10 +17,11 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                    
 
