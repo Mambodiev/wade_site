1b840700350a1b483c04a099a6d568ed70299978
mambodieu <mambodieu@gmail.com>
2022-02-24 12:44:57 -0400

    succes collect static file

--- ouest/settings.py
+++ ouest/settings.py
@@ -121,7 +121,7 @@ STATIC_URL='/static/'
 STATIC_ROOT = BASE_DIR / 'wade_site/staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- ouest/urls.py
+++ ouest/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from blog import views
 
 
@@ -17,10 +16,10 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
