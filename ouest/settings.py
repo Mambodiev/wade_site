9b60e10fdf78b76021752428ecd3d1293da7f599
mambodieu <mambodieu@gmail.com>
2022-02-26 14:13:56 -0400

    master

--- ouest/settings.py
+++ ouest/settings.py
@@ -10,7 +10,7 @@ from pathlib import Path
 # environ.Env.read_env()
 ALLOWED_HOSTS = ["ouestsenegal.herokuapp.com", '127.0.0.1']
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
