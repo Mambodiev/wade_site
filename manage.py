f8213119e85518e62d4738dcd687ca7bf34b20d9
mambodieu <mambodieu@gmail.com>
2022-05-04 15:44:19 +0300

    heroku deploy with dennis

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 import dotenv
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
+    # dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ouest.settings')
     try:
         from django.core.management import execute_from_command_line
