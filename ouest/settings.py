758549b17d20dac4946701a254d67686d8276338
mambodieu <mambodieu@gmail.com>
2022-05-04 05:26:34 +0300

    heroku deploy with dennis

--- ouest/settings.py
+++ ouest/settings.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-DEBUG = str(os.environ.get('DEBUG')) == '1'
+DEBUG = True
+# DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 # Allowed host for production
 # ALLOWED_HOSTS = ['ouestsenegal.herokuapp.com']
